74a90d564671da25b9ddc1ea2ad9df2e1fd9e55d
Yutaka Ishikawa <yutaka_ishikawa@nii.ac.jp>
2021-04-18 16:40:55 +0900

    for the gneral operation environment.

--- mpitest/run/run-fugaku-coll-vbg.sh
+++ mpitest/run/run-fugaku-coll-vbg.sh
@@ -11,27 +11,23 @@
 #	PJM --mpi "max-proc-per-node=2"
 #	PJM --mpi "max-proc-per-node=4"
 #PJM --mpi "max-proc-per-node=4"
-#PJM -L "elapse=00:00:20"
-#PJM -L "rscunit=rscunit_ft01,rscgrp=eap-small,jobenv=linux2"
+#PJM -L "elapse=00:01:00"
+#PJM -L "rscunit=rscunit_ft01,rscgrp=small"
 #PJM -L proc-core=unlimited
 #------- Program execution -------#
 
 MPIOPT="-of results/vbg/%n.%j.out -oferr results/vbg/%n.%j.err"
-VBGLOAD="env LD_PRELOAD=../../build/libmpi_vbg.so"
+export PATH=~/mpich-tofu/bin:$PATH
+export MPICH_HOME=~/mpich-tofu/
+which mpich_exec
+export UTF_BG_LOAD=1
 
-#export UTF_BG_CONFIRM=1
-#export UTF_BG_DBG=1
-#export UTF_BG_DISABLE=1
-#export UTF_BG_INITWAIT=10000
-#export UTF_BG_BARRIER=1
-export UTF_BG_UTFPROGRESS=1
+export MPIR_CVAR_CH4_OFI_CAPABILITY_SETS_DEBUG=1
+export MPICH_TOFU_SHOW_PARAMS=1
 
-echo "UTF_BG_CONFIRM" $UTF_BG_CONFIRM
-echo "UTF_BG_DBG" $UTF_BG_DB
-echo "UTF_BG_DISABLE" $UTF_BG_DISABLE
-echo "UTF_BG_INITWAIT" $UTF_BG_INITWAIT
-echo "UTF_BG_BARRIER" $UTF_BG_BARRIER
-echo "UTF_BG_UTFPROGRESS" $UTF_BG_UTFPROGRESS
+export UTF_BG_CONFIRM=1
+echo "UTF_BG_LOAD= " $UTF_BG_LOAD
+echo "UTF_BG_CONFIRM= " $UTF_BG_CONFIRM
 
 echo "Barrier"
 mpich_exec $MPIOPT $VBGLOAD ../src/test_collective_vbg -s 0x1
@@ -49,3 +45,4 @@ echo "AllReduce"
 mpich_exec $MPIOPT $VBGLOAD ../src/test_collective_vbg -s 0x8
 
 exit
+
